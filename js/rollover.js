af4b0646741036ecb5c8b0408645e2d6e0db86a2
dollysingh <dollysingh3192@gmail.com>
2017-06-12 23:38:28 +0530

    slider functioning started

--- js/rollover.js
+++ js/rollover.js
@@ -11,14 +11,38 @@ $(document).ready(function() {
             }
         }
         for (var i = 0; i < arr.length; i++) {
-        	var fli = "Div_"+currentli;
+            var fli = "Div_" + currentli;
             if (arr[i] == fli) {
                 $('#Div_' + currentli).css("display", "block");
-            }
-            else
-            {
-            	 $('#'+arr[i]).css("display", "none");
+            } else {
+                $('#' + arr[i]).css("display", "none");
             }
         }
     });
+    var slideIndex = 7;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        if (n > x.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = x.length
+        }
+        var pwidth = $(".header").width();
+        console.log(pwidth);
+        var from = pwidth / n;
+        for (i = from; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        for (i = 0; i < from; i++) {
+            x[slideIndex - 1].style.display = "block";
+        }
+    }
 });
